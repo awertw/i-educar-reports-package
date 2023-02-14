163ca7c79fc97713b8a800b463804201912d400e
kesllan <kesllan@gmail.com>
2023-02-13 22:03:47 -0300

    add try catch pra prevenir erro na migration

--- database/migrations/2023_02_10_192000_add_conference_faults_report_menu.php
+++ database/migrations/2023_02_10_192000_add_conference_faults_report_menu.php
@@ -2,6 +2,7 @@
 
 use App\Menu;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\QueryException;
 
 class AddConferenceFaultsReportMenu extends Migration
 {
@@ -12,22 +13,31 @@ class AddConferenceFaultsReportMenu extends Migration
      */
     public function up()
     {
-        $parentId = Menu::query()->where('old', 999922)->firstOrFail()->getKey();
+        try {
+            $parentId = Menu::query()->where('old', 999922)->firstOrFail()->getKey();
+        } catch (\Exception $e) {
+            $parentId = 0;
+        }
+
         $counter = 0;
         while(true){
             $id = $parentId + $counter;
-            if(!Menu::query()->where('id', $id)->exists()){
-                Menu::query()->create([
-                    'id' => $id,
-                    'parent_id' => $parentId,
-                    'title' => 'Relatório de conferência de faltas',
-                    'description' => null,
-                    'link' => '/module/Reports/ConferenceFaults',
-                    'order' => 0,
-                    'old' => 230210,
-                    'process' => 230210,
-                ]);
-                break;
+            try {
+                if(!Menu::query()->where('id', $id)->exists()){
+                    Menu::query()->create([
+                        'id' => $id,
+                        'parent_id' => $parentId,
+                        'title' => 'Relatório de conferência de faltas',
+                        'description' => null,
+                        'link' => '/module/Reports/ConferenceFaults',
+                        'order' => 0,
+                        'old' => 230210,
+                        'process' => 230210,
+                    ]);
+                    break;
+                }
+            } catch (QueryException $e) {
+                // Do nothing and continue with the loop
             }
             $counter++;
         }
