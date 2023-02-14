e93857ed542e8ce1c929e8b35636f9da358bb3dd
kesllan <kesllan@gmail.com>
2023-02-14 17:24:20 -0300

    migrate menu faltas inserção manual

--- database/migrations/2023_02_10_192000_add_conference_faults_report_menu.php
+++ database/migrations/2023_02_10_192000_add_conference_faults_report_menu.php
@@ -12,27 +12,15 @@ class AddConferenceFaultsReportMenu extends Migration
      */
     public function up()
     {
-        $parentId = Menu::query()->where('old', 999922)->firstOrFail()->getKey();
-        $counter = 0;
-        while(true){
-            $id = $parentId + $counter;
-            try {
-                Menu::query()->create([
-                    'id' => $id,
-                    'parent_id' => $parentId,
-                    'title' => 'Relatório de conferência de faltas',
-                    'description' => null,
-                    'link' => '/module/Reports/ConferenceFaults',
-                    'order' => 0,
-                    'old' => 230213,
-                    'process' => 230213,
-                ]);
-                break;
-            } catch (\Exception $e) {
-                $counter++;
-                continue;
-            }
-        }
+        Menu::query()->create([
+            'parent_id' => 326,
+            'title' => 'Relatório de conferência de faltas',
+            'description' => null,
+            'link' => '/module/Reports/ConferenceFaults',
+            'order' => 1,
+            'old' => NULL,
+            'process' => 999809,
+        ]);
     }
 
     /**
@@ -42,6 +30,6 @@ class AddConferenceFaultsReportMenu extends Migration
      */
     public function down()
     {
-        Menu::query()->where('process', 230210)->delete();
+        Menu::query()->where('process', 999809)->delete();
     }
 }
