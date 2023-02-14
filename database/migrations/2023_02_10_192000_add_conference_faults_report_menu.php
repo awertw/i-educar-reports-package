11afa76e97df3bb8d5932d5e26cb57feb8f5e186
kesllan <kesllan@gmail.com>
2023-02-14 10:34:34 -0300

    migrate faltas v6

--- database/migrations/2023_02_10_192000_add_conference_faults_report_menu.php
+++ database/migrations/2023_02_10_192000_add_conference_faults_report_menu.php
@@ -12,36 +12,25 @@ class AddConferenceFaultsReportMenu extends Migration
      */
     public function up()
     {
-        
         $parentId = Menu::query()->where('old', 999922)->firstOrFail()->getKey();
         $counter = 0;
-
-        while (true) {
+        while(true){
             $id = $parentId + $counter;
-            try {                                
-                    Menu::query()->create([                        
-                        'parent_id' => $id,
-                        'title' => 'Relatório de conferência de faltas',
-                        'description' => null,
-                        'link' => '/module/Reports/ConferenceFaults',
-                        'order' => 0,
-                        'old' => 230210,
-                        'process' => 230210,
-                    ]);
-                    $counter++;                    
-            }            
-            catch (\Exception $e) {                
-                $id = $parentId + $counter;
-                Menu::query()->create([                        
-                    'parent_id' => $id,
+            try {
+                Menu::query()->create([
+                    'id' => $id,
+                    'parent_id' => $parentId,
                     'title' => 'Relatório de conferência de faltas',
                     'description' => null,
                     'link' => '/module/Reports/ConferenceFaults',
                     'order' => 0,
-                    'old' => 230210,
-                    'process' => 230210,
+                    'old' => 230213,
+                    'process' => 230213,
                 ]);
+                break;
+            } catch (\Exception $e) {
                 $counter++;
+                continue;
             }
         }
     }
