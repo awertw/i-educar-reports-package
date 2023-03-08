1a7f926d86abd9c25bfff92a447cfcea43ca4de6
kesllan <kesllan@gmail.com>
2023-03-07 23:44:42 -0300

    Filtrar Deficiencia Correção Controller

--- ieducar/Views/StudentsWithDisabilitiesController.php
+++ ieducar/Views/StudentsWithDisabilitiesController.php
@@ -1,16 +1,16 @@
 <?php
 
-class TeacherReportCardController extends Portabilis_Controller_ReportCoreController
+class StudentsWithDisabilitiesController extends Portabilis_Controller_ReportCoreController
 {
     /**
      * @var int
      */
-    protected $_processoAp = 999205;
+    protected $_processoAp = 999227;
 
     /**
      * @var string
      */
-    protected $_titulo = 'Boletim do Professor';
+    protected $_titulo = 'Relatório de Alunos com Deficiência';
 
     /**
      * @inheritdoc
@@ -21,8 +21,8 @@ class TeacherReportCardController extends Portabilis_Controller_ReportCoreContro
 
         Portabilis_View_Helper_Application::loadStylesheet($this, 'intranet/styles/localizacaoSistema.css');
 
-        $this->breadcrumb('Boletim do professor', [
-            'educar_index.php' => 'Escola'
+        $this->breadcrumb('Relatório de alunos com Deficiência', [
+            'educar_index.php' => 'Escola',
         ]);
     }
 
@@ -31,47 +31,14 @@ class TeacherReportCardController extends Portabilis_Controller_ReportCoreContro
      */
     public function form()
     {
-        $this->inputsHelper()->dynamic(['ano', 'instituicao', 'escola', 'curso', 'serie']);
+        $this->inputsHelper()->dynamic(['ano', 'instituicao', 'escola']);
+        $this->inputsHelper()->dynamic('curso', ['required' => false]);
+        $this->inputsHelper()->dynamic('serie', ['required' => false]);
         $this->inputsHelper()->dynamic('turma', ['required' => false]);
-        $this->inputsHelper()->dynamic('componenteCurricular', ['required' => false]);
-        $options = [
-            'label' => 'Situação',
-            'resources' => [
-                0 => 'Todos',
-                1 => 'Aprovado',
-                2 => 'Reprovado',
-                3 => 'Cursando',
-                4 => 'Transferido',
-                5 => 'Reclassificado',
-                6 => 'Abandono',
-                7 => 'Em exame',
-                8 => 'Aprovado após exame',
-                10 => 'Aprovado sem exame'
-            ],
-            'required' => false,
-            'value' => 0
-        ];
-        $this->inputsHelper()->select('situacao', $options);
+        $this->inputsHelper()->dynamic('situacaoMatricula');        
+        $this->inputsHelper()->dynamic(['deficiencia'], ['required' => false]);        
 
-        $options = [
-            'label' => 'Orientação da página',
-            'resources' => [1 => 'Paisagem', 2 => 'Retrato'],
-            'required' => false,
-            'value' => 1
-        ];
-        $this->inputsHelper()->select('orientacao', $options);
-        $this->inputsHelper()->checkbox('buscar_professor', ['label' => 'Buscar professor alocado?']);
-        $options = [
-            'label' => 'Professor(a):',
-            'required' => false,
-            'size' => 30
-        ];
-        $this->inputsHelper()->text('professor', $options);
-        $this->inputsHelper()->simpleSearchServidor(null, ['required' => false, 'label' => 'Professor(a): ', 'size' => 30]);
-        $this->campoNumero('linha', 'Linhas em branco', 0, 2, 2, true);
-        $this->inputsHelper()->checkbox('emitir_assinaturas', ['label' => 'Emitir assinaturas no rodapé?']);
-        $this->inputsHelper()->checkbox('data_manual', ['label' => 'Data manual?']);
-        $this->loadResourceAssets($this->getDispatcher());
+
     }
 
     /**
@@ -85,23 +52,19 @@ class TeacherReportCardController extends Portabilis_Controller_ReportCoreContro
         $this->report->addArg('curso', (int) $this->getRequest()->ref_cod_curso);
         $this->report->addArg('serie', (int) $this->getRequest()->ref_cod_serie);
         $this->report->addArg('turma', (int) $this->getRequest()->ref_cod_turma);
-        $this->report->addArg('disciplina', (int) $this->getRequest()->ref_cod_componente_curricular);
-        $this->report->addArg('situacao', (int) $this->getRequest()->situacao);
-        $this->report->addArg('professor', $this->getRequest()->professor);
-        $this->report->addArg('servidor', $this->getRequest()->servidor);
-        $this->report->addArg('linha', ((int) $this->getRequest()->ref_cod_componente_curricular == 0?0:(int) $this->getRequest()->linha));
-        $this->report->addArg('orientacao', (int) $this->getRequest()->orientacao);
-        $this->report->addArg('emitir_assinaturas', (bool) $this->getRequest()->emitir_assinaturas);
-        $this->report->addArg('data_manual', (bool) $this->getRequest()->data_manual);
+        $this->report->addArg('situacao', (int) $this->getRequest()->situacao_matricula_id);        
+        $this->report->addArg('disabilities', (int) $this->getRequest()->deficiencia_id);
+        //dd($this->getRequest()->deficiencia_id);
+
     }
 
     /**
-     * @return TeacherReportCardReport
+     * @return StudentsWithDisabilitiesReport
      *
      * @throws Exception
      */
     public function report()
     {
-        return new TeacherReportCardReport();
+        return new StudentsWithDisabilitiesReport();
     }
 }
